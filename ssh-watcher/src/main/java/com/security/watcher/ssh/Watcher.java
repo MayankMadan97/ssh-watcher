63035338dfb90ad12f177f3211b29181351fee07
Mayank Madan <mk@dispension.com>
2025-08-02 14:41:34 -0300

    Added base watcher class to run in bg

---
+++ ssh-watcher/src/main/java/com/security/watcher/ssh/Watcher.java
@@ -0,0 +1,38 @@
+package com.security.watcher.ssh;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.regex.Pattern;
+
+public class Watcher {
+
+    Path filePath;
+    ProcessBuilder process;
+
+    public Watcher(Path filePath) {
+        this.filePath = filePath;
+    }
+
+    public Watcher(String command) {
+        if (command != null && command.trim().length() > 0) {
+            this.process = new ProcessBuilder(command.trim().split(" "));
+        } else {
+            throw new IllegalArgumentException("Commands empty");
+        }
+    }
+
+    public void startWatching() throws IOException {
+        Pattern authLogPAttern = Pattern.compile("sshd.*(Failed|Accepted).*from");
+        if (this.filePath != null) {
+            // Firstly, check if the file exists
+            if (Files.exists(filePath)) {
+                Files.lines(filePath).filter(line -> authLogPAttern.matcher(line).matches())
+                        .forEach(line -> System.out.println(line));
+            } else {
+                throw new IOException("File at path \"" + filePath.toString() + "\" doesn't exist");
+            }
+        }
+    }
+
+}
